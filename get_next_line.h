07808dc3856ee5cc7a31f29ed7160ff0d4babc85
David Soto Garcia <dsoto-ga@c1r15s2.42madrid.com>
2026-07-15 17:21:24 +0200

    feat: add  files structure

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsoto-ga <dsoto-ga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/14 15:51:52 by dsoto-ga          #+#    #+#             */
+/*   Updated: 2026/07/14 15:52:28 by dsoto-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
